81749c8ab986eb4c4595ce1afe3a914c09051b1a
geonwooPark <white0581@naver.com>
2024-07-26 00:49:58 +0900

    feat: Comment API 작업중

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -19,7 +19,7 @@ export const createComment: RequestHandler = async (req, res) => {
   const { id, content } = req.body;
 
   try {
-    const updatedComment = await Comment.findOneAndUpdate(
+    const updatedComment = await Comment.findOneAndUpdate<CommentDocument>(
       { parrentId: id },
       {
         $push: {
@@ -42,6 +42,57 @@ export const createComment: RequestHandler = async (req, res) => {
     );
 
     return res.status(201).json(updatedComment);
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const deleteComment: RequestHandler = async (req, res) => {
+  const { id, commentId } = req.body;
+
+  try {
+    // const replyComment = await ReplyComment.find<CommentDocument>(
+    //   {
+    //     parrentId: id,
+    //   },
+    //   {
+    //     user: {
+    //       $elemMatch: {
+    //         commentId,
+    //       },
+    //     },
+    //   }
+    // );
+
+    // await Comment.findOneAndUpdate<CommentDocument>(
+    //   {
+    //     parrentId: id,
+    //   },
+    //   replyComment[0].comments.length !== 0
+    //     ? { $set: { "comments.$[elem].isDeleted": true } }
+    //     : { $pull: { comments: { commentId } } },
+    //   { arrayFilters: [{ "elem.commentId": commentId }], new: true }
+    // );
+
+    return res.status(200).json({ id });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const updateComment: RequestHandler = async (req, res) => {
+  const { id, commentId, content } = req.body;
+
+  try {
+    await Comment.findOneAndUpdate<CommentDocument>(
+      {
+        parrentId: id,
+      },
+      { $set: { "comments.$[elem].content": content } },
+      { arrayFilters: [{ "elem.commentId": commentId }] }
+    );
+
+    return res.status(200).json({ message: "댓글 수정 성공!" });
   } catch (error) {
     return res.status(500).json({ error: "Internal server error" });
   }
--- src/routers/comment.router.ts
+++ src/routers/comment.router.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { createComment, getComments } from "../controllers/comment.controller";
+import {
+  createComment,
+  deleteComment,
+  getComments,
+  updateComment,
+} from "../controllers/comment.controller";
 
 const router = Router();
 
 router.get("/", getComments);
 router.post("/", createComment);
-// router.delete("/:id", deleteComment);
-// router.patch("/:id", updateComment);
+router.delete("/:id", deleteComment);
+router.patch("/:id", updateComment);
 
 export default router;
