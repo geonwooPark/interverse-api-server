c777cfef062f542a569f6df9bb74d4e100e3fafd
박건우 <white0581@naver.com>
2024-08-15 12:21:22 +0900

    chore: https 세팅

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ require("dotenv").config();
 import "./db";
 import express from "express";
 import fs from "fs";
-import http from "http";
 import https from "https";
 import cors from "cors";
 import postingRouter from "./routers/posting.router";
@@ -13,18 +12,6 @@ import checklistRouter from "./routers/checklist.router";
 import userRouter from "./routers/user.router";
 
 const app = express();
-
-const options = {
-  ca: fs.readFileSync(
-    `/etc/letsencrypt/live/${process.env.DOMAIN as string}/fullchain.pem`
-  ),
-  key: fs.readFileSync(
-    `/etc/letsencrypt/live/${process.env.DOMAIN as string}/privkey.pem`
-  ),
-  cert: fs.readFileSync(
-    `/etc/letsencrypt/live/${process.env.DOMAIN as string}/cert.pem`
-  ),
-};
 
 app.use(express.static("public"));
 app.use(
@@ -41,8 +28,24 @@ app.use("/like", likeRouter);
 app.use("/checklist", checklistRouter);
 app.use("/user", userRouter);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("서버 실행중..");
-});
-http.createServer(app).listen(3000);
-https.createServer(options, app).listen(443);
+if (process.env.NODE_ENV == "production") {
+  const options = {
+    ca: fs.readFileSync(
+      `/etc/letsencrypt/live/${process.env.DOMAIN as string}/fullchain.pem`
+    ),
+    key: fs.readFileSync(
+      `/etc/letsencrypt/live/${process.env.DOMAIN as string}/privkey.pem`
+    ),
+    cert: fs.readFileSync(
+      `/etc/letsencrypt/live/${process.env.DOMAIN as string}/cert.pem`
+    ),
+  };
+
+  https.createServer(options, app).listen(process.env.PORT || 3000, () => {
+    console.log(`${process.env.PORT || 3000}PORT 실행중..`);
+  });
+} else {
+  app.listen(process.env.PORT || 3000, () => {
+    console.log("서버 실행중...");
+  });
+}
