ba24a40dc3b3944e7f6e63c013f753eddd4e44f8
박건우 <white0581@naver.com>
2024-09-02 22:53:06 +0900

    Chore: ssl 설정중

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,6 @@ import userRouter from "./routers/user.router";
 
 const app = express();
 
-// app.use(express.static("public"));
 app.use(
   cors({ origin: ["http://localhost:3000", "https://www.ventileco.site"] })
 );
@@ -32,24 +31,24 @@ app.use("/like", likeRouter);
 app.use("/checklist", checklistRouter);
 app.use("/user", userRouter);
 
-if (process.env.NODE_ENV == "production") {
-  const options = {
-    ca: fs.readFileSync(
-      `/etc/letsencrypt/live/${process.env.DOMAIN as string}/fullchain.pem`
-    ),
-    key: fs.readFileSync(
-      `/etc/letsencrypt/live/${process.env.DOMAIN as string}/privkey.pem`
-    ),
-    cert: fs.readFileSync(
-      `/etc/letsencrypt/live/${process.env.DOMAIN as string}/cert.pem`
-    ),
-  };
+// if (process.env.NODE_ENV == "production") {
+//   const options = {
+//     ca: fs.readFileSync(
+//       `/etc/letsencrypt/live/${process.env.DOMAIN as string}/fullchain.pem`
+//     ),
+//     key: fs.readFileSync(
+//       `/etc/letsencrypt/live/${process.env.DOMAIN as string}/privkey.pem`
+//     ),
+//     cert: fs.readFileSync(
+//       `/etc/letsencrypt/live/${process.env.DOMAIN as string}/cert.pem`
+//     ),
+//   };
 
-  https.createServer(options, app).listen(process.env.PORT || 443, () => {
-    console.log(`${process.env.PORT || 443}PORT 실행중..`);
-  });
-} else {
-  app.listen(process.env.PORT || 3000, () => {
-    console.log(`${process.env.PORT}PORT 실행중..`);
-  });
-}
+// https.createServer(options, app).listen(process.env.PORT || 443, () => {
+//   console.log(`${process.env.PORT || 443}PORT 실행중..`);
+// });
+// } else {
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`${process.env.PORT}PORT 실행중..`);
+});
+// }
