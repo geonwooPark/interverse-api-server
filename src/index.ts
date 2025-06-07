1c080f251c41967d3aaf9a113f96c07fd434bd93
박건우 <white0581@naver.com>
2025-06-07 18:17:56 +0900

    Chore: https 관련 로직 제거

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ require("dotenv").config();
 import "./module-alias";
 import "./db";
 import express from "express";
-import fs from "fs";
-import https from "https";
 import cors from "cors";
 import authRouter from "@routers/auth.router";
 import roomsRouter from "@routers/rooms.router";
@@ -14,7 +12,7 @@ const app = express();
 
 const allowedOrigins =
   process.env.NODE_ENV === "production"
-    ? ["https://yourdomain.com"]
+    ? ["https://interverse.site"]
     : ["http://localhost:5173"];
 
 app.use(cors({ origin: allowedOrigins }));
@@ -31,25 +29,7 @@ app.get("/api-docs.json", (req, res) => {
   res.json(swaggerSpec);
 });
 
-if (process.env.NODE_ENV == "production") {
-  const options = {
-    ca: fs.readFileSync(
-      `/etc/letsencrypt/live/${process.env.DOMAIN as string}/fullchain.pem`
-    ),
-    key: fs.readFileSync(
-      `/etc/letsencrypt/live/${process.env.DOMAIN as string}/privkey.pem`
-    ),
-    cert: fs.readFileSync(
-      `/etc/letsencrypt/live/${process.env.DOMAIN as string}/cert.pem`
-    ),
-  };
-
-  https.createServer(options, app).listen(process.env.PORT || 443, () => {
-    console.log(`${process.env.PORT || 443}PORT 실행중..`);
-  });
-} else {
-  app.listen(process.env.PORT || 8000, () => {
-    console.log(`${process.env.PORT || 8000}PORT 실행중..`);
-    console.log("📘 Swagger UI: http://localhost:8000/api-docs");
-  });
-}
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`${process.env.PORT || 8000}PORT 실행중..`);
+  console.log("📘 Swagger UI: http://localhost:8000/api-docs");
+});
